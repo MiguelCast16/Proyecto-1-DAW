933319f7ee0e24f826cae0395d8d31cf7aa07c75
MiguelCast16 <48912492+MiguelCast16@users.noreply.github.com>
2019-05-21 09:17:17 +0200

    Add files via upload

---
+++ INSERT PRUEBA.sql
@@ -0,0 +1,14 @@
+INSERT INTO ALUMNOS VALUES ('504847602Z','Mike','Castle',65489725,'suspenso@gmail.com');
+INSERT INTO ALUMNOS VALUES ('504454602N','Nikki','Castle',63489725,'aprobado@gmail.com');
+INSERT INTO ALUMNOS VALUES ('504445982K','Alvaro','Alce',63480101,'apuestas@gmail.com');
+
+INSERT INTO TUTOR VALUES ('61289895P','Noelia','ArgoUml','Entornos@gmail.com');
+INSERT INTO TUTOR VALUES ('60089895A','Turi','Box','Sistemas@gmail.com');
+
+INSERT INTO CICLOS VALUES (1,'DAW','Grado Superior','Informatica','NO SE','NO SE','NO SE,','A1');
+INSERT INTO CICLOS VALUES (2,'TAFAD','Grado Superior','Deporte','NO SE','NO SE','NO SE,','A2');
+
+INSERT INTO EMPRESAS VALUES ('1','PRUEBA1','Miguel','5048P','Castillo','Google','España','Madrid','Madrid','Calle Ciudad','45223',633040404,481464,'Madrid','05/01/2019','Deporte','1');
+INSERT INTO EMPRESAS VALUES ('2','PRUEBA2','Fran','5047P','Morales','Amazon','España','Madrid','Madrid','Calle Pais','45224',633040555,481464,'Madrid','09/01/2019','Informatica','1');
+
+INSERT INTO CENTRO VALUES ('1','David','Perez','5047P','Valle Del Miro','España','Madrid','Calle Pais','45227','NOSE',633040668,789456123,'NO SE QUE ES EL DAT');
